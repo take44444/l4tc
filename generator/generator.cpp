915c8b8b656b29ddc916def57c19b01936f52def
take44444 <take44444.general@gmail.com>
2022-08-26 19:45:29 +0900

    complete generator

--- generator/generator.cpp
+++ generator/generator.cpp
@@ -2,6 +2,23 @@
 
 namespace generator {
   const std::string param_reg_names[6] = {"rdi", "rsi", "rdx", "rcx", "r8",  "r9"};
+
+  std::shared_ptr<TypeFunc> create_func_type(std::shared_ptr<ASTFuncDeclaration> fd) {
+    std::vector<std::shared_ptr<EvalType>> type_args;
+    for (std::shared_ptr<ASTSimpleDeclaration> d: fd->declarator->args) {
+      type_args.push_back(
+        create_type(d->declarator, create_base_type(d->type_spec))
+      );
+    }
+    return std::make_shared<TypeFunc>(
+      type_args,
+      create_type(
+        fd->declarator->declarator,
+        create_base_type(fd->type_spec)
+      )
+    );
+  }
+
   std::shared_ptr<EvalType> create_type(std::shared_ptr<ASTDeclarator> d, std::shared_ptr<EvalType> base_type) {
     // TODO: pointer
     return base_type;
@@ -22,13 +39,13 @@ namespace generator {
     return nullptr;
   }
 
-  void generator_sub(std::shared_ptr<AST> ast, std::shared_ptr<Context> ctx, std::string &code) {
+  void generate_sub(std::shared_ptr<AST> ast, std::shared_ptr<Context> ctx, std::string &code) {
     if (typeid(*ast) == typeid(ASTTranslationUnit)) {
       std::shared_ptr<ASTTranslationUnit> n = std::dynamic_pointer_cast<ASTTranslationUnit>(ast);
       code += ".intel_syntax noprefix\n"; // use intel syntax
-      code += ".text\n";
+      code += ".text\n"; // text section
       for (std::shared_ptr<AST> d: n->external_declarations) {
-        generator_sub(d, ctx, code);
+        generate_sub(d, ctx, code);
       }
       return;
     }
@@ -43,46 +60,38 @@ namespace generator {
     }
     if (typeid(*ast) == typeid(ASTFuncDef)) {
       std::shared_ptr<ASTFuncDef> n = std::dynamic_pointer_cast<ASTFuncDef>(ast);
-      std::vector<std::shared_ptr<EvalType>> type_args;
+      std::shared_ptr<ASTFuncDeclaration> fd = n->declaration;
+      std::string func_name = std::string(fd->declarator->declarator->op->sv);
+      std::shared_ptr<TypeFunc> tf = create_func_type(fd);
       std::vector<std::string> name_args;
-      std::string func_name = std::string(n->declaration->declarator->declarator->op->sv);
-      for (
-        std::shared_ptr<ASTSimpleDeclaration> d: n->declaration->declarator->args
-      ) {
-        type_args.push_back(create_type(d->declarator, create_base_type(d->type_spec)));
+      if (fd->declarator->args.size() > 6) {
+        // TODO: the maximum number of arguments of function is 6 in lup
+        assert(false);
+      }
+      for (std::shared_ptr<ASTSimpleDeclaration> d: fd->declarator->args) {
         name_args.push_back(std::string(d->declarator->op->sv));
       }
-      ctx->add_func(
-        func_name,
-        create_type(
-          n->declaration->declarator->declarator,
-          create_base_type(n->declaration->type_spec)
-        ),
-        type_args
-      );
+      ctx->add_global_var(func_name, tf);
       code += ".global " + func_name + "\n";
       code += func_name + ":\n";
       assert(ctx->rsp == 0); // here is global
       ctx->start_scope(); // remember rsp value
       code += "push rbp\n";
       code += "mov rbp, rsp\n";
-      ctx->rsp = 0; // rsp == rbp
-      if (type_args.size() > 6) {
-        // TODO: the maximum number of arguments of function is 6
-      }
-      // sub number of params * 8 from rsp
-      code += "sub rsp, " + std::to_string((int)type_args.size() * 8) + "\n";
-      for (int i=0; i < (int)type_args.size(); i++) {
+      ctx->rsp = 0; // now rsp == rbp
+      // push arguments
+      code += "sub rsp, " + std::to_string((int)name_args.size() * 8) + "\n";
+      for (int i=0; i < (int)name_args.size(); i++) {
         // all size of vars are 8 byte (64bit) in this lupc
         ctx->rsp -= 8;
         // add vars in function arguments to local vars
-        ctx->add_local_var(name_args[i], type_args[i]);
+        ctx->add_local_var(name_args[i], tf->type_args[i]);
         code += "mov [rbp - " + std::to_string(-ctx->rsp) + "], " + param_reg_names[i] + "\n";
       }
-      generator_sub(n->body, ctx, code);
-      // pop arguments from stack
-      code += "add rsp " + std::to_string((int)type_args.size() * 8) + "\n";
-      ctx->rsp += (int)type_args.size() * 8;
+      generate_sub(n->body, ctx, code);
+      // pop arguments
+      ctx->rsp += (int)name_args.size() * 8;
+      code += "add rsp " + std::to_string((int)name_args.size() * 8) + "\n";
       ctx->end_scope(); // check rsp
       code += "mov rsp, rbp\n";
       code += "pop rbp\n";
@@ -103,64 +112,72 @@ namespace generator {
     }
     if (typeid(*ast) == typeid(ASTCompoundStmt)) {
       std::shared_ptr<ASTCompoundStmt> n = std::dynamic_pointer_cast<ASTCompoundStmt>(ast);
-      int saved_rsp1 = ctx->rsp;
+      ctx->start_scope(); // remember rsp value
       for (std::shared_ptr<AST> stmt: n->items) {
-        if (typeid(*stmt) == typeid(ASTDeclaration)) generator_sub(stmt, ctx, code);
+        if (typeid(*stmt) == typeid(ASTDeclaration)) generate_sub(stmt, ctx, code);
       }
-      int saved_rsp2 = ctx->rsp;
+      int saved_rsp = ctx->rsp;
       for (std::shared_ptr<AST> stmt: n->items) {
         if (typeid(*stmt) == typeid(ASTDeclaration)) continue;
         if (typeid(*stmt) == typeid(ASTCompoundStmt)) ctx->start_scope();
-        generator_sub(stmt, ctx, code);
+        generate_sub(stmt, ctx, code);
         if (typeid(*stmt) == typeid(ASTCompoundStmt)) ctx->end_scope();
       }
-      assert(ctx->rsp == saved_rsp2);
-      code += "add rsp, " + std::to_string(saved_rsp1 - saved_rsp2) + "\n";
-      ctx->rsp += saved_rsp1 - saved_rsp2;
+      assert(ctx->rsp == saved_rsp);
+      ctx->rsp += ctx->saved_rsp - saved_rsp;
+      code += "add rsp, " + std::to_string(ctx->saved_rsp - saved_rsp) + "\n";
+      ctx->end_scope();
       return;
     }
     if (typeid(*ast) == typeid(ASTExprStmt)) {
       std::shared_ptr<ASTExprStmt> n = std::dynamic_pointer_cast<ASTExprStmt>(ast);
-      generator_sub(n->expr, ctx, code);
+      generate_sub(n->expr, ctx, code);
       ctx->rsp += 8;
       code += "pop r10\n"; // pop the value that need not be evaluate
       return;
     }
     if (typeid(*ast) == typeid(ASTReturnStmt)) {
       std::shared_ptr<ASTReturnStmt> n = std::dynamic_pointer_cast<ASTReturnStmt>(ast);
-      generator_sub(n->expr, ctx, code);
+      generate_sub(n->expr, ctx, code);
       ctx->rsp += 8;
       code += "pop rax\n"; // set return value
-      code += "add rsp, " + std::to_string(ctx->func_saved_rsp() - ctx->rsp) + "\n";
       code += "mov rsp, rbp\n";
       code += "pop rbp\n";
       code += "ret\n";
+      return;
     }
     // if (typeid(*ast) == typeid(ASTBreakStmt)) {
-    //   std::string label = ctx->get_loop_info().label_break;
+    //   std::string label = ctx->get_loop().label_break;
+    //   // TODO rsp
     //   if (!label.length()) {
     //     // TODO error
+    //     assert(false);
     //   }
     //   code += "jmp L" + label;
     //   return;
     // }
     // if (typeid(*ast) == typeid(ASTContinueStmt)) {
-    //   std::string label = ctx->get_loop_info().label_continue;
+    //   std::string label = ctx->get_loop().label_continue;
+    //   // TODO rsp
     //   if (!label.length()) {
     //     // TODO error
+    //     assert(false);
     //   }
     //   code += "jmp L" + label;
     //   return;
     // }
     if (typeid(*ast) == typeid(ASTAssignExpr)) {
       std::shared_ptr<ASTAssignExpr> n = std::dynamic_pointer_cast<ASTAssignExpr>(ast);
-      generator_sub(n->right, ctx, code);
-      generator_sub(n->left, ctx, code);
+      generate_sub(n->right, ctx, code);
+      generate_sub(n->left, ctx, code);
       if (!n->left->is_assignable) {
         // TODO error
+        assert(false);
       }
-      if (typeid(n->left->eval_type) != typeid(n->right->eval_type)) {
+      // TODO recursive check for type of pointer, function
+      if (typeid(*(n->left->eval_type)) != typeid(*(n->right->eval_type))) {
         // TODO error
+        assert(false);
       }
       code += "pop r11\n";
       code += "pop r10\n";
@@ -168,7 +185,7 @@ namespace generator {
       code += "mov [r10], r11\n";
       code += "mov r11, [r10]\n";
       code += "push r11\n";
-      ctx->rsp += 8;
+      ctx->rsp += 8; // 2 pop 1 push
       n->eval_type = n->left->eval_type;
       n->is_assignable = false;
     }
@@ -182,13 +199,15 @@ namespace generator {
     // if (typeid(*ast) == typeid(ASTShiftExpr)) {}
     if (typeid(*ast) == typeid(ASTAdditiveExpr)) {
       std::shared_ptr<ASTAdditiveExpr> n = std::dynamic_pointer_cast<ASTAdditiveExpr>(ast);
-      generator_sub(n->left, ctx, code);
-      generator_sub(n->right, ctx, code);
-      if (typeid(n->left->eval_type) != typeid(TypeNum)) {
+      generate_sub(n->left, ctx, code);
+      generate_sub(n->right, ctx, code);
+      if (typeid(*(n->left->eval_type)) != typeid(TypeNum)) {
         // TODO error
+        assert(false);
       }
-      if (typeid(n->right->eval_type) != typeid(TypeNum)) {
+      if (typeid(*(n->right->eval_type)) != typeid(TypeNum)) {
         // TODO error
+        assert(false);
       }
       code += "pop r11\n";
       code += "pop r10\n";
@@ -196,19 +215,21 @@ namespace generator {
       if (n->right->is_assignable) code += "mov r11 [r11]\n";
       code += "add r10, r11\n";
       code += "push r10\n";
-      ctx->rsp += 8;
+      ctx->rsp += 8; // 2 pop and 1 push
       n->eval_type = n->left->eval_type;
       n->is_assignable = false;
     }
     if (typeid(*ast) == typeid(ASTMultiplicativeExpr)) {
       std::shared_ptr<ASTMultiplicativeExpr> n = std::dynamic_pointer_cast<ASTMultiplicativeExpr>(ast);
-      generator_sub(n->left, ctx, code);
-      generator_sub(n->right, ctx, code);
-      if (typeid(n->left->eval_type) != typeid(TypeNum)) {
+      generate_sub(n->left, ctx, code);
+      generate_sub(n->right, ctx, code);
+      if (typeid(*(n->left->eval_type)) != typeid(TypeNum)) {
         // TODO error
+        assert(false);
       }
-      if (typeid(n->right->eval_type) != typeid(TypeNum)) {
+      if (typeid(*(n->right->eval_type)) != typeid(TypeNum)) {
         // TODO error
+        assert(false);
       }
       code += "pop r11\n";
       code += "pop r10\n";
@@ -216,45 +237,87 @@ namespace generator {
       if (n->right->is_assignable) code += "mov r11 [r11]\n";
       code += "imul r10, r11\n";
       code += "push r10\n";
-      ctx->rsp += 8;
+      ctx->rsp += 8; // 2 pop and 1 push
       n->eval_type = n->left->eval_type;
       n->is_assignable = false;
     }
     if (typeid(*ast) == typeid(ASTFuncCallExpr)) {
       std::shared_ptr<ASTFuncCallExpr> n = std::dynamic_pointer_cast<ASTFuncCallExpr>(ast);
-      generator_sub(n->primary, ctx, code);
-      // ctx->get_func(n->primary->)
+      generate_sub(n->primary, ctx, code);
+      if (typeid(*(n->primary->eval_type)) != typeid(TypeFunc)) {
+        // TODO error
+        assert(false);
+      }
+      std::shared_ptr<TypeFunc> tf = std::dynamic_pointer_cast<TypeFunc>(n->primary->eval_type);
+      if (n->args.size() != tf->type_args.size()) {
+        // TODO error
+        assert(false);
+      }
+      for (int i=0; i < (int)n->args.size(); i++) {
+        generate_sub(n->args[i], ctx, code);
+        if (typeid(*(n->args[i])) != typeid(*(tf->type_args[i]))) {
+          // TODO error
+          assert(false);
+        }
+      }
+      for (int i=(int)n->args.size()-1; i >= 0; i--) {
+        code += "pop " + param_reg_names[i] + "\n";
+      }
+      ctx->rsp += (int)n->args.size() * 8;
+      code += "pop rax\n";
+      // rsp needs to be aligned when call
+      if (!ctx->is_rsp_aligned()) code += "sub rsp 8\n";
+      code += "call rax\n";
+      if (!ctx->is_rsp_aligned()) code += "add rsp 8\n";
+      code += "push rax\n";
+      n->eval_type = tf->ret_type;
+      n->is_assignable = false;
+      return;
     }
     if (typeid(*ast) == typeid(ASTPrimaryExpr)) {
       std::shared_ptr<ASTPrimaryExpr> n = std::dynamic_pointer_cast<ASTPrimaryExpr>(ast);
-      generator_sub(n->expr, ctx, code);
+      generate_sub(n->expr, ctx, code);
       n->eval_type = n->expr->eval_type;
       n->is_assignable = n->expr->is_assignable;
+      return;
     }
     if (typeid(*ast) == typeid(ASTSimpleExpr)) {
       std::shared_ptr<ASTSimpleExpr> n = std::dynamic_pointer_cast<ASTSimpleExpr>(ast);
       if (n->op->type == Ident) {
-        LocalVarInfo lvi = ctx->get_local_var_info(n->op->sv);
-        n->eval_type = lvi.type;
-        n->is_assignable = true;
-        code += "lea r10, [rbp - " + std::to_string(lvi.offset) + "]\n";
-        code += "push r10\n";
-        ctx->rsp -= 8;
+        std::shared_ptr<LocalVar> lvi = ctx->get_local_var(n->op->sv);
+        if (lvi) {
+          code += "lea r10, [rbp - " + std::to_string(lvi->offset) + "]\n";
+          code += "push r10\n";
+          ctx->rsp -= 8;
+          n->eval_type = lvi->type;
+          n->is_assignable = true;
+          return;
+        }
+        std::shared_ptr<GlobalVar> gvi = ctx->get_global_var(n->op->sv);
+        if (gvi) {
+          code += ".global" + gvi->name + "\n";
+          code += "mov r10, [rip + " + gvi->name + "@GOTPCREL]\n";
+          code += "push r10\n";
+          ctx->rsp -= 8;
+          n->eval_type = gvi->type;
+          n->is_assignable = typeid(*(n->eval_type)) != typeid(TypeFunc);
+          return;
+        }
       } else if (n->op->type == NumberConstant) {
-        n->eval_type = std::make_shared<TypeNum>();
-        n->is_assignable = false;
         code += "mov r10, " + std::string(n->op->sv) + "\n";
         code += "push r10\n";
         ctx->rsp -= 8;
+        n->eval_type = std::make_shared<TypeNum>();
+        n->is_assignable = false;
+        return;
       }
-      return;
     }
   }
 
-  std::string generator(std::shared_ptr<AST> ast) {
+  std::string generate(std::shared_ptr<AST> ast) {
     std::string ret;
     std::shared_ptr<Context> context = std::make_shared<Context>();
-    generator_sub(ast, context, ret);
+    generate_sub(ast, context, ret);
     return ret;
   }
 }
--- generator/generator.hpp
+++ generator/generator.hpp
@@ -17,10 +17,10 @@ namespace generator {
     TypeNum() : EvalType() {}
   };
 
-  class TypeVoid : public EvalType {
-    public:
-    TypeVoid() : EvalType() {}
-  };
+  // class TypeVoid : public EvalType {
+  //   public:
+  //   TypeVoid() : EvalType() {}
+  // };
 
   class TypePointer : public EvalType {
     public:
@@ -28,39 +28,84 @@ namespace generator {
     TypePointer(std::shared_ptr<EvalType> of) : EvalType(), pointer_of(of) {}
   };
 
-  class LoopInfo {
+  class TypeFunc : public EvalType {
     public:
-    std::string label_break;
-    std::string label_continue;
-    LoopInfo(int i) {
-      if (i < 0) {
-        label_break = "";
-        label_continue = "";
-        return;
-      }
-      label_break = std::to_string(i) + "B";
-      label_continue = std::to_string(i) + "C";
-    }
+    std::vector<std::shared_ptr<EvalType>> type_args;
+    std::shared_ptr<EvalType> ret_type;
+    TypeFunc(std::vector<std::shared_ptr<EvalType>> &ta, std::shared_ptr<EvalType> rt)
+    : type_args(ta), ret_type(rt) {}
+  };
+
+  // class LoopInfo {
+  //   public:
+  //   std::string label_break;
+  //   std::string label_continue;
+  //   LoopInfo(int i) {
+  //     if (i < 0) {
+  //       label_break = "";
+  //       label_continue = "";
+  //       return;
+  //     }
+  //     label_break = std::to_string(i) + "B";
+  //     label_continue = std::to_string(i) + "C";
+  //   }
+  // };
+
+  class GlobalVar {
+    public:
+    std::string name;
+    std::shared_ptr<EvalType> type;
+    GlobalVar(std::string n, std::shared_ptr<EvalType> t) : name(n), type(t) {}
+  };
+
+  class LocalVar {
+    public:
+    int offset;
+    std::shared_ptr<EvalType> type;
+    LocalVar(int o, std::shared_ptr<EvalType> t) : offset(o), type(t) {}
   };
 
   class Context {
     public:
     int rsp;
-    Context() : rsp(0) {}
-    void add_global_var(std::string key, std::shared_ptr<EvalType> type) {}
-    void add_func(std::string key, std::shared_ptr<EvalType> type, std::vector<std::shared_ptr<EvalType>> &type_args) {}
+    int saved_rsp;
+    std::vector<std::map<std::string, std::shared_ptr<LocalVar>>> scopes_local_vars;
+    std::map<std::string, std::shared_ptr<GlobalVar>> global_vars;
+    Context() : rsp(0), saved_rsp(0) {}
+    void add_global_var(std::string key, std::shared_ptr<EvalType> type) {
+      global_vars.at(key) = std::make_shared<GlobalVar>(key, type);
+    }
     void add_local_var(std::string key, std::shared_ptr<EvalType> type) {
-      local_var.rbp_minus = -rsp;
+      std::shared_ptr<LocalVar> lv = std::make_shared<LocalVar>(-rsp, type);
+      // TODO insert lv
+    }
+    std::shared_ptr<GlobalVar> get_global_var(std::string_view key) {
+      auto it = global_vars.find(std::string(key));
+      if (it == global_vars.end()) return nullptr;
+      return it->second;
+    }
+    std::shared_ptr<LocalVar> get_local_var(std::string_view key) {
+      for (int i=(int)scopes_local_vars.size()-1; i >= 0; i--) {
+        auto it = scopes_local_vars[i].find(std::string(key));
+        if (it == scopes_local_vars[i].end()) continue;
+        return it->second;
+      }
+      return nullptr;
     }
     void start_scope() {
-      // store rsp
+      saved_rsp = rsp;
+      scopes_local_vars.push_back(
+        std::map<std::string, std::shared_ptr<LocalVar>>()
+      );
     }
     void end_scope() {
-      // check rsp is stored value
+      assert(saved_rsp == rsp);
+      scopes_local_vars.pop_back();
     }
-    LoopInfo get_loop_info() {
-      return LoopInfo(-1);
-    }
+
+    // LoopInfo get_loop() {
+    //   return LoopInfo(-1);
+    // }
     bool is_rsp_aligned() {
       return !(rsp & 0xF);
     }
