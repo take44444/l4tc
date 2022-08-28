a41bc5e2b595cd7ddd60c61593fd29feea5b4b5f
take44444 <take44444.general@gmail.com>
2022-08-28 17:22:21 +0900

    improve shell script

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+if [ $# -lt 1 ]; then
+  echo 'need argument' >&2
+  exit 1
+fi
 make
-rm -f main.S
-cat main.lup | ./lupc 1> main.S
-gcc -o main main.S
-./main
+rm -f $1.S
+cat main.lup | ./lupc 1> $1.S
+gcc -o $1 $1.S
+./$1
 echo $?
